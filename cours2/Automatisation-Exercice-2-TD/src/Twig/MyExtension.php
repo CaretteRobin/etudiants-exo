6aae82f5603d947043dc8164e4b00bde3bb3b8f5
Robin <robincrtt05@gmail.com>
2026-01-19 11:00:07 +0100

    chore(lint): add phpstan phpcs and eslint tooling

--- cours2/Automatisation-Exercice-2-TD/src/Twig/MyExtension.php
+++ cours2/Automatisation-Exercice-2-TD/src/Twig/MyExtension.php
@@ -3,7 +3,6 @@
 namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class MyExtension extends AbstractExtension
@@ -13,39 +12,42 @@ class MyExtension extends AbstractExtension
         return 'my-extension';
     }
 
-		public function getFunctions(): array {
-			return [
-				new TwigFunction('getEnvironmentVariable', [$this, 'getEnvironmentVariable']),
-				new TwigFunction('getViteAssets', [$this, 'getViteAssets']),
-			];
-		}
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('getEnvironmentVariable', [$this, 'getEnvironmentVariable']),
+            new TwigFunction('getViteAssets', [$this, 'getViteAssets']),
+        ];
+    }
 
-		public function getEnvironmentVariable(string $varName): ?string {
-			return $_ENV[$varName] ?? null;
-		}
+    public function getEnvironmentVariable(string $varName): ?string
+    {
+        return $_ENV[$varName] ?? null;
+    }
 
-		public function manifest() {
-			$json_file_path = __DIR__ . '/../../public/build/.vite/manifest.json';
-			if (file_exists($json_file_path)) {
-				$json_data = file_get_contents($json_file_path);
+    public function manifest()
+    {
+        $jsonFilePath = __DIR__ . '/../../public/build/.vite/manifest.json';
+        if (file_exists($jsonFilePath)) {
+            $jsonData = file_get_contents($jsonFilePath);
 
-				return json_decode($json_data, true);
-			}
+            return json_decode($jsonData, true);
+        }
 
-			return '';
-		}
+        return '';
+    }
 
-		public function getViteAssets(): string {
-			if ($_ENV['ENV'] === 'prod') {
-				$manifest = $this->manifest();
-				if (!$manifest) {
-					return '';
-				}
+    public function getViteAssets(): string
+    {
+        if ($_ENV['ENV'] === 'prod') {
+            $manifest = $this->manifest();
+            if (!$manifest) {
+                return '';
+            }
 
-				return 'xxxxx'; 	// todo : return hashed filenames (css and js) in production, use manifest.json content to dynamise
-			}
-			else {
-				return 'yyyyyy'; // todo : return dev server (app.js and vite client) in development
-			}
-		}
+            return 'xxxxx'; // todo : return hashed filenames (css and js) in production, use manifest.json content to dynamise
+        }
+
+        return 'yyyyyy'; // todo : return dev server (app.js and vite client) in development
+    }
 }
