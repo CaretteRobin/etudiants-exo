38ddfa10154c741ca8b074d8a0434646f95b29c0
SAKIROO <noe.franoux@gmail.com>
2026-03-14 10:55:09 +0100

    builder: add mysql query builder

--- cours6-design_pattern/design-pattern-main/builder/public/index.php
+++ cours6-design_pattern/design-pattern-main/builder/public/index.php
@@ -1,7 +1,31 @@
 <?php
-require('../vendor/autoload.php');
+require __DIR__ . '/../vendor/autoload.php';
 
+use App\MySQLQueryBuilder;
 
-# TODO: Creer un QueryBuilder
-# Ecrire une requête en chainant des methodes
-# Afficher la requête
+$firstQuery = (new MySQLQueryBuilder())
+    ->select(['id', 'name', 'email'])
+    ->from('users')
+    ->where('active = 1')
+    ->build();
+
+$secondQuery = (new MySQLQueryBuilder())
+    ->select(['title', 'author'])
+    ->from('books')
+    ->where("category = 'php'")
+    ->where('published = 1')
+    ->build();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Builder</title>
+</head>
+<body>
+    <ul>
+        <li><?= $firstQuery; ?></li>
+        <li><?= $secondQuery; ?></li>
+    </ul>
+</body>
+</html>
