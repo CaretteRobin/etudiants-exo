17074bda669856f2a7eb0fc41531b1c9df3c01c3
SAKIROO <noe.franoux@gmail.com>
2026-03-14 10:55:08 +0100

    decorator: add computer decorators

---
+++ cours6-design_pattern/design-pattern-main/decorator/app/GPUDecorator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+class GPUDecorator implements Computer
+{
+    public function __construct(
+        private Computer $computer
+    ) {}
+
+    public function getPrice(): int
+    {
+        return $this->computer->getPrice() + 250;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->computer->getDescription() . ' with GPU';
+    }
+}
